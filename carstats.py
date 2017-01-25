44d25c12df63671956bcdeb619923267348175a0
Jason Chen <jason.chen@cwmsolution.com>
2017-01-25 17:42:44 -0500

    initial commit

--- carstats.py
+++ carstats.py
@@ -13,8 +13,9 @@ import matplotlib
 from bs4 import BeautifulSoup
 from urllib2 import build_opener, HTTPCookieProcessor
 from pandas.stats.api import ols
+import webbrowser
 
-matplotlib.use('ggplot')
+matplotlib.style.use('ggplot')
 
 # mercedes c300
 # url = 'http://www.autotrader.ca/cars/mercedes-benz/c-class/on/toronto/?prx=100&prv=Ontario&loc=m5p3h6&sts=New-Used&hprc=True&wcp=True&rcs=0&rcp=1000'
@@ -40,7 +41,7 @@ class CarStats(object):
         soup = BeautifulSoup(html, "lxml")
         self.df = self._parse_html(soup)
         self.car_model = '{}_{}_{}'.format(url_split[3], url_split[4], url_split[5])
-        self.name = '{} {}'.format(url_split[4],url_split[5])
+        self.name = '{} {}'.format(url_split[4], url_split[5])
         self.ols_summary = ''
         self.report_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'reports')
 
@@ -54,13 +55,35 @@ class CarStats(object):
         self.ols_summary = res.summary
         return res
 
-    def gen_graph_price_vs_km(self, show=False):
+    def gen_graph_km_vs_price(self, show=False):
         fig_name = '{}_age_vs_km.png'.format(self.car_model)
         return self.gen_scatter_plot(x='km', y='price', fig_name=fig_name)
 
     def gen_graph_age_vs_price(self, show=False):
         fig_name = '{}_age_vs_price.png'.format(self.car_model)
         return self.gen_scatter_plot(x='age', y='price', fig_name=fig_name)
+
+    def html_report(self, show=True):
+        html_file_path = os.path.join(self.report_path, self.car_model + '.html')
+        f = open(html_file_path, 'wb')
+        html = """
+        <h1>{report_name} Statistics</h1>
+        <h2>Linear Regression</h2>
+        <p>{ols}</p>
+        <img src="{img_1}" alt="Opps...can't display image">
+        <img src="{img_2}" alt="Opps...can't display image">
+
+        """.format(report_name=self.name,
+                   ols=self.gen_ols(),
+                   img_1=os.path.basename(self.gen_graph_age_vs_price(show=False)),
+                   img_2=os.path.basename(self.gen_graph_km_vs_price(show=False)))
+        f.write(html)
+        f.close()
+        if show:
+            webbrowser.open('file://' + html_file_path)
+
+    def predict(self, age, km, html=True, show=True):
+        pass
 
     def gen_scatter_plot(self, x, y, fig_name, show=False, figsize=(6, 4)):
         """
@@ -111,6 +134,6 @@ class CarStats(object):
 
 
 if __name__ == '__main__':
-    mercedes_c = CarStats(
+    mercedes_e = CarStats(
         'http://www.autotrader.ca/cars/mercedes-benz/e-class/on/toronto/?prx=100&prv=Ontario&loc=m5p3h6&sts=New-Used&hprc=True&wcp=True&rcs=0&rcp=1000')
-    mercedes_c.gen_graph_age_vs_price(show=True)
+    mercedes_e.html_report(show=True)
